5d7503bb1522a52db7c27fbd097e02f581599d54
Charle Biveu Doehl <charles_doehl@estudante.sesisenai.org.br>
2024-09-29 01:19:16 -0300

    [M3S02] Ex. 05 - Incluindo permissão do usuário no payload JWT

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,16 +1,28 @@
-require('dotenv').config();
 const { verify } = require('jsonwebtoken');
 
+require('dotenv').config(); // Carregar variáveis do .env
+
+/**
+ * Middleware para garantir que o usuário tem uma permissão específica
+ * @param {string} permitirApenas Permissão necessária para acessar a rota
+ * @returns {import('express').RequestHandler}
+ */
 function garantirAutenticao(permitirApenas) {
   return (request, response, next) => {
     const { authorization } = request.headers;
-    if (!authorization)
-      return response.status(401).json({ message: 'Token não encontrado!' });
 
+    if (!authorization) {
+      return response.status(401).json({ message: 'Token não encontrado!' });
+    }
+
+    // Extrair o token do cabeçalho "Authorization"
     const [, token] = authorization.split(' ');
+
     try {
-      const decoded = verify(token, process.env.JWT_SECRET); // Verifica usando a chave do .env
-      const { sub, permissao } = decoded;
+      // Verificar e decodificar o token
+      const decoded = verify(token, process.env.JWT_SECRET);
+
+      const { sub, permissao } = decoded; // Extraindo a permissão do payload do token
 
       if (permitirApenas !== permissao) {
         return response
@@ -18,12 +30,21 @@ function garantirAutenticao(permitirApenas) {
           .json({ message: 'Acesso negado: permissão insuficiente!' });
       }
 
-      request.user = { id: sub, permissao };
-      next();
+      // Adiciona os dados do usuário no request para serem usados nas rotas
+      request.user = {
+        id: sub,
+        permissao,
+      };
+
+      next(); // Passa para a próxima função (rota)
     } catch (error) {
-      return response.status(401).json({ message: 'Token inválido!' });
+      return response
+        .status(401)
+        .json({ message: 'Token inválido ou expirado!' });
     }
   };
 }
 
-module.exports = { garantirAutenticao };
+module.exports = {
+  garantirAutenticao,
+};
